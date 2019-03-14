8e838fc981b165798a24f1062fc9f153bb860cd3
Tee <47378564+javawtee@users.noreply.github.com>
2019-03-14 02:15:01 -0700

    Clean up App.js

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,83 +1,18 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom'
+import React, { Component } from "react";
+import {Route} from "react-router-dom"
 
-import LoginForm from './layouts/forms/LoginForm'
-import UserConsole from './layouts/UserConsole'
+import HomePage from "./layouts/HomePage";
+import UserConsole from "./layouts/UserConsole"
 
-import './App.css'
+import "./App.css"
 
 class App extends Component {
-  state = {
-    login: false,
-    signup: false,
-    session_username: '', //should pass a session as prop, this is just for test purpose
-  }
-
-  componentWillMount(){
-    //
-  }
-
-  componentDidUpdate(){
-    if(this.state.session_username !== ''){
-      sessionStorage.setItem('username', this.state.session_username)
-      window.location.href = '/console'
-    }
-  }
-
-  formClick = (form) => {
-    var login = !this.state.login
-    var signup = !this.state.signup
-    if(form === 'login')
-      this.setState({login, signup: false})
-    else
-      this.setState({signup, login: false})
-  }
-
-  onLogin = (e, userID, password) => {
-    e.preventDefault();
-    if(userID !== '' && password !== ''){
-        fetch('/testpage', 
-          {
-          method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({userID, password})
-        })
-        .then(res => res.json())
-        .then(payload => {
-        if(payload.numOfResults === 0)
-          alert('userID or password is incorrect')
-        else
-          this.setState({session_username: payload.results[0]})
-            //this.setState({userName: payload.results[0]}) //should redirect to UserConsole here
-        }) // whenever setState is called, this component will be re-rendered
-    } else 
-        alert('userID or password cannot be empty')
-  }
-
   render() {
-    const {login, signup} = this.state
     return (
       <div className="App" >
-        <Route exact path='/' render={() => {
-          return (
-            <div id='home-container'>
-              <div id='home-nav'>
-              </div>
-              <div id='home-div'>
-                <div className='home-common-div' onClick={() =>this.formClick('login')}>Login</div>
-                <div className='login-form home-common-div' style={{display: login? 'block': 'none' }}>
-                  <LoginForm onLogin={(e,userID,password) => this.onLogin(e,userID,password)}/>
-                </div>
-                <div className='home-common-div' onClick={() =>this.formClick('signup')}>Sign up</div>
-                <div className='signup-form home-common-div' style={{display: signup? 'block': 'none' }}>Sign up Form</div>
-              </div>
-            </div>
-          )
-        }}/>
-        <Route path='/console' component={UserConsole}/>
+        <Route exact path="/" component={() => <HomePage content="login"/>} />
+        <Route exact path="/signUp" component={() => <HomePage content="signUp"/>} />
+        <Route path="/console" component={UserConsole}/>
       </div>
     );
   }
