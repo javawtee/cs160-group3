1fd1b8b6304f967ce147dc9d4025029ff74b63ea
Ada La <adala345@gmail.com>
2019-03-20 01:05:20 -0700

    add multiple items

--- server/client/src/layouts/Restaurant.js
+++ server/client/src/layouts/Restaurant.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
 
+import '../App.css';
+
 export class Restaurant extends Component { 
   state = { 
+    numItems: 1
+  }
+
+  increaseItemCount = () => {
+    this.setState({
+      numItems: this.state.numItems + 1
+    });  
+  }
+
+  renderItemFields = () => {
+    let fields = [];
+
+    for (let i = 0; i < this.state.numItems; i++) {
+      fields.push( 
+        <form id='form'>     
+          <input className='input' type="text"   
+          placeholder="Item name"/>
+          
+          <input className='input' type="text"  
+          placeholder="Quantity"/>          
+               
+          <input className='input' type="text" 
+          placeholder="Delivery address"/>
+        </form>
+      )
+    }
+
+    return fields;
   }
   
-  render () {                                   
-      return (
+  render () {                                    
+    return (
+      <div>
         <div>
-             <div id='restaurantContainer'>
-                  <form id='form'>       
-                      <input className='input' type="text"   
-                       placeholder="First Name"/>
-                      <input className='input' type="text"  
-                       placeholder="Last Name"/>          
-                      <input className='input' type="text"  
-                       placeholder="Email"/>          
-                      <input className='input' type="password" 
-                       placeholder="Password"/>
-                      <button id='submit'>Sign Up</button>
-                  </form>
-             </div>
+          <div id='restaurantContainer'>
+            <div id='home-common-div'>
+              <p>This is the restaurant view</p>
+            </div>
+
+            {this.renderItemFields()}
+
+            
+            <button id='addItem' onClick={this.increaseItemCount}>Add Item</button>
+            <button id='submit'>Place order</button>
+          </div>
         </div>
+      </div>
       )
-   }
+  }
 }
 
 export default Restaurant;
