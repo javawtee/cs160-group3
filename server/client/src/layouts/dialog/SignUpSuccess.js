c808e239987a5794a66fe71fd1add3fd1b465302
Tee <47378564+javawtee@users.noreply.github.com>
2019-03-21 01:52:38 -0700

    Create SignUpSuccess.js
    Dialog appears when sign up is successful

---
+++ server/client/src/layouts/dialog/SignUpSuccess.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Dialog} from '@material-ui/core';
+
+export class SignUpSuccess extends Component {
+  render() {
+    const {onClose, ...other} = this.props;
+    return (
+        <Dialog id="signUpDialog" {...other}>
+            <div className="row" style={{padding: "55px"}}>
+                <div className="col">
+                    <h4>SUBMITTED SIGN-UP FORM SUCCESSFULLY</h4>
+                    <hr/>
+                    <div>
+                        Your form will be reviewed soon...<br/>
+                        If it is approved, you will be notified via your registered email<br/>
+                        and your account is eligible to login
+                    </div>
+                </div>
+                <div className="w-100"></div>
+                <div className="col">
+                    <br/>
+                    <br/>
+                    <button className="btn btn-primary" onClick={onClose}>Close</button>
+                </div>
+            </div>
+        </Dialog>
+    )
+  }
+}
+
+export default SignUpSuccess
